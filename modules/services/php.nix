2d3a9c29da159e93daa9d607a23d571f06c645ae
wriver4 <wriver4@gmail.com>
2025-07-15 09:55:59 -0400

    update dev and server tools

--- modules/services/php.nix
+++ modules/services/php.nix
@@ -6,6 +6,13 @@
     php84
     php84Packages.composer
     php84Packages.composer-local-repo-plugin
+    php84Packages.phpunit
+    php84Packages.php-cs-fixer
+    php84Packages.phpstan
+    php84Packages.php-cs-fixer-composer-installer
+    php84Packages.php-cs-fixer-fixer
+    mago
+    
   ];
 };
 }
