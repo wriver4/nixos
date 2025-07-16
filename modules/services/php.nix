25675c0cb4b434e2ae0a9c230f862db204430c49
wriver4 <wriver4@gmail.com>
2025-07-16 09:05:02 -0400

    tweak

--- modules/services/php.nix
+++ modules/services/php.nix
@@ -6,11 +6,6 @@
     php84
     php84Packages.composer
     php84Packages.composer-local-repo-plugin
-    php84Packages.phpunit
-    php84Packages.php-cs-fixer
-    php84Packages.phpstan
-    php84Packages.php-cs-fixer-composer-installer
-    php84Packages.php-cs-fixer-fixer
   ];
 };
 }
