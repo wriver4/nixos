58f9a201ebaaae863fd28122ae39ff68cd7d24c7
wriver4 <wriver4@gmail.com>
2025-08-01 09:49:52 -0400

    add npm

--- modules/services/servers.nix
+++ modules/services/servers.nix
@@ -11,6 +11,7 @@
         # sudo env PATH=$PATH:/nix/store/wfxq6w9bkp5dcfr8yb6789b0w7128gnb-nodejs-20.18.1/bin /nix/store/sni4zssnggfnqfz40i92n40vxg6msssw-pm2-5.4.2/lib/node_modules/pm2/bin/pm2 startup systemd -u mark --hp /home/mark
         #PM2][ERROR] Failure when trying to write startup script
         #EROFS: read-only file system, open '/etc/systemd/system/pm2-mark.service'
+    nodejs_24
     
   ];
   services.mysql = {
