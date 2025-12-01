3941ea80a2b4f2a79a667243413039722f1d4797
wriver4 <wriver4@gmail.com>
2025-12-01 17:55:47 -0500

    remove more gnome stuff

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -5,68 +5,63 @@
 {
   config = {
 
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
 
-  services.gnome.gnome-keyring.enable = true;
-  services.gvfs.enable = true;
+    services.gnome.gnome-keyring.enable = true;
+    services.gvfs.enable = true;
 
-  programs.seahorse.enable = true;
-  programs.nautilus-open-any-terminal = {
-    enable = true;
-    terminal = "terminator";
-  };
+    programs.seahorse.enable = true;
+    programs.nautilus-open-any-terminal = {
+      enable = true;
+      terminal = "terminator";
+    };
 
+    environment.systemPackages = with pkgs; [ 
+      brasero
+      gnome-builder
+      gnome-usage
+      cheese
+      dconf-editor
+      gnome-tweaks
+      wl-clipboard
+      wl-clipboard-x11
+      xclip
+      xsel
+      gnomeExtensions.tophat
+      gnomeExtensions.force-quit
+      gnomeExtensions.panel-date-format
+      gnomeExtensions.dock-from-dash
+      gnomeExtensions.hide-activities-button
+      gnomeExtensions.places-status-indicator
+      gnomeExtensions.tweaks-in-system-menu
+      gnomeExtensions.xwayland-indicator
+      # gnomeExtensions.systemd-manager
+      unstable.gnomeExtensions.keep-pinned-apps-in-appgrid
+      gnomeExtensions.public-ip-address
+      nautilus-python
+      nautilus-open-any-terminal
+    ];
+    #exclude some Gnome Packages
+    environment.gnome.excludePackages = with pkgs;[
+      geary # email
+      epiphany # web browser
+      gnome-maps
+      gnome-weather
+      gnome-tour
+      gnome-contacts
+      gnome-help
+    ];
 
-    
-  environment.systemPackages = with pkgs; [ 
-    brasero
-    gnome-builder
-    gnome-usage
-    cheese
-    dconf-editor
-    gnome-tweaks
-    wl-clipboard
-    wl-clipboard-x11
-    xclip
-    xsel
-    gnomeExtensions.tophat
-    gnomeExtensions.force-quit
-    gnomeExtensions.panel-date-format
-    gnomeExtensions.dock-from-dash
-    gnomeExtensions.hide-activities-button
-    gnomeExtensions.places-status-indicator
-    gnomeExtensions.tweaks-in-system-menu
-    gnomeExtensions.xwayland-indicator
-    # gnomeExtensions.systemd-manager
-    unstable.gnomeExtensions.keep-pinned-apps-in-appgrid
-    gnomeExtensions.public-ip-address
-    nautilus-python
-    nautilus-open-any-terminal
-  ];
-  #exclude some Gnome Packages
-  environment.gnome.excludePackages = with pkgs;[
-    geary # email
-    epiphany # web browser
-    gnome-maps
-    gnome-weather
-    gnome-tour
-    gnome-contacts
-    gnome-help
-  ];
-
-
-  #fontDir.enable = true;
-  fonts.packages = with pkgs; [
-    corefonts
-    dejavu_fonts
-    freefont_ttf
-    google-fonts
-    inconsolata
-    liberation_ttf
-    ubuntu_font_family
-  ];
-  
-
+    #fontDir.enable = true;
+    fonts.packages = with pkgs; [
+      corefonts
+      dejavu_fonts
+      freefont_ttf
+      google-fonts
+      inconsolata
+      liberation_ttf
+      ubuntu_font_family
+    ];
   };
 }
