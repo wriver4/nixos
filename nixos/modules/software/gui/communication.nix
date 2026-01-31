23f725393c5c966572f7855ed0c259f3018840f6
wriver4 <wriver4@users.noreply.github.com>
2026-01-31 14:10:14 -0500

    look for anydesk solution

--- nixos/modules/software/gui/communication.nix
+++ nixos/modules/software/gui/communication.nix
@@ -3,10 +3,22 @@
 {
   config = {
     environment.systemPackages = with pkgs; [
+      # AnyDesk remote desktop (package includes its own systemd service)
+      anydesk
+      # AnyDesk X11 mode desktop entry (for Wayland compatibility issues)
+      (pkgs.makeDesktopItem {
+        name = "anydesk-x11";
+        desktopName = "AnyDesk (X11)";
+        exec = "env GDK_BACKEND=x11 anydesk %u";
+        icon = "anydesk";
+        comment = "AnyDesk Remote Desktop (forced X11 mode)";
+        categories = [ "Network" "RemoteAccess" ];
+        mimeTypes = [ "x-scheme-handler/anydesk" ];
+      })
+
       thunderbird
       unstable.nextcloud-talk-desktop
       #remote desktops
-      # unstable.anydesk #acting funky
       remmina
       #viber
       #teamspeak3client
