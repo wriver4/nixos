befea2540c1c732ee8b3687246e2d1b636fabc0b
wriver4 <wriver4@users.noreply.github.com>
2026-04-07 09:42:02 -0400

    add vivify with desktop entry for Nautilus open-with

--- nixos/modules/software/gui/text_editors.nix
+++ nixos/modules/software/gui/text_editors.nix
@@ -1,5 +1,20 @@
 { config, pkgs, ... }:
 
+let
+  vivifyDesktop = pkgs.makeDesktopItem {
+    name = "vivify";
+    desktopName = "Vivify";
+    comment = "View Markdown and Jupyter Notebooks in the browser";
+    exec = "viv %f";
+    mimeTypes = [
+      "text/markdown"
+      "text/x-markdown"
+      "application/x-ipynb+json"
+    ];
+    categories = [ "Utility" "TextEditor" "Viewer" ];
+    noDisplay = false;
+  };
+in
 {
   config = {
     environment.systemPackages = with pkgs; [
@@ -11,6 +26,7 @@
       #stirling-pdf
       zettlr
       vivify # Markdown/Jupyter viewer in the browser
+      vivifyDesktop
     ];
   };
 }
